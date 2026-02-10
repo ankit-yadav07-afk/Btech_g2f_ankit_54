159b2116338d2cdf3d008b0848dc518922b80df8
ankit-yadav07-afk <y66ankit@gmail.com>
2026-02-10 12:26:53 +0530

    Add files via upload

---
+++ grade.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int a;
+    printf(" enter the makrs ");
+    scanf("%d",&a);
+    if(a>90 && a<100)
+    {
+        printf("grade A %d",a);
+    
+}
+ if(a>80 && a<90)
+    {
+        printf("grade B %d",a);
+    
+}
+ if(a>60 && a<80)
+    {
+        printf("grade C %d",a);
+    
+}
+ if( a<60)
+    {
+        printf("grade D %d",a);
+    
+}
+return 0;
+}
+
